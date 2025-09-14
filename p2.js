ab5b7145c798e608fa9012218832ea40ddc1798e
abhishekkumar177 <abhishekumar1772004@gmail.com>
2025-09-15 00:26:26 +0530

    init

--- p2.js
+++ p2.js
@@ -9,3 +9,17 @@ gsap.from(".mobile-screen-container", {
     duration: 1.2,
     ease: "power3.out"
 });
+
+// Animate the gradient blinds background
+gsap.fromTo(".blind", {
+    opacity: 0,
+    y: "-50%"
+}, {
+    opacity: 1,
+    y: "0%",
+    duration: 1,
+    stagger: 0.1,
+    ease: "power2.out",
+    repeat: -1, // Infinite loop
+    yoyo: true // Go back and forth
+});
